926adc727cd11b0536d4ccc77473d2899185c649
HoangPhuc2k <phphuc2k@gmail.com>
2020-11-27 10:52:08 +0700

    Update 27/11 10:51 Model User. DPContext

--- WebApp/Areas/Admin/Models/UserModel.cs
+++ WebApp/Areas/Admin/Models/UserModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,30 @@ namespace WebApp.Areas.Admin.Models
 {
     public class UserModel
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nhập Đầy Đủ Thông Tin")]
+        [StringLength(maximumLength:200, ErrorMessage = "Độ Dài Phải Qua 200 Ký Tự",MinimumLength = 16)]
+        [Display(Name = "Account Name")]
+        public string AccountName { get; set; }
+        [StringLength(maximumLength:200,ErrorMessage ="Độ dài không phù hợp",MinimumLength = 12)]
+        [Display(Name = "Acount Password")]
+        public string AccountPassword { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        [StringLength(maximumLength:200,ErrorMessage ="Độ dài không phù hợp")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Address")]
+        [StringLength(maximumLength: 1000, ErrorMessage = "Độ dài không phù hợp",MinimumLength = 16)]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        [StringLength(maximumLength: 10, ErrorMessage = "Độ dài không phù hợp", MinimumLength = 10)]
+        public string Phone { get; set; }
+        public int RolesId { get; set; }
+        [ForeignKey("RolesId")]
+        public virtual RolesModel Roles { get; set; }
     }
 }
