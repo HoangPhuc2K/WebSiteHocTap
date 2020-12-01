86579357c7d28bf8346d17a8b4cd7a27008f38ba
vanduchuy09321 <40846748+vanduchuy09321@users.noreply.github.com>
2020-12-01 12:55:30 +0700

    Course, Coach, Lesson

---
+++ WebApp/Areas/Admin/Models/CoachModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Areas.Admin.Models
+{
+    public class CoachModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nhập Đầy Đủ Thông Tin")]
+        [StringLength(maximumLength: 200, ErrorMessage = "Độ Dài Phải Qua 200 Ký Tự", MinimumLength = 16)]
+        [Display(Name = "Account Name")]
+        public string AccountName { get; set; }
+
+        [StringLength(maximumLength: 200, ErrorMessage = "Độ dài không phù hợp", MinimumLength = 12)]
+        [Display(Name = "Acount Password")]
+        public string AccountPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [StringLength(maximumLength: 200, ErrorMessage = "Độ dài không phù hợp")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        [StringLength(maximumLength: 1000, ErrorMessage = "Độ dài không phù hợp", MinimumLength = 16)]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [StringLength(maximumLength: 10, ErrorMessage = "Độ dài không phù hợp", MinimumLength = 10)]
+        public string Phone { get; set; }
+
+        public int IdRoles { get; set; }
+
+        public ICollection<LessonModel> Lesson { get; set; }
+    }
+}
--- WebApp/Areas/Admin/Models/CourseModel.cs
+++ WebApp/Areas/Admin/Models/CourseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,21 @@ namespace WebApp.Areas.Admin.Models
 {
     public class CourseModel
     {
+        [Key]
+        public int IdCourse { set; get; }
+
+        [Required]
+        [Display(Name = "Title")]
+        [StringLength(maximumLength: 200, ErrorMessage = "Độ dài không phù hợp")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        [StringLength(maximumLength: 300, ErrorMessage = "Độ dài không phù hợp")]
+        public string Description { get; set; }
+
+        public ICollection<LessonModel> Lesson { get; set; }
+
     }
 }
+    
